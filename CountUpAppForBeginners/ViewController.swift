78eb31fbc21a143357baaa2253a2520be194b93c
techiro <techiromen0@gmail.com>
2020-08-04 20:53:50 +0900

    ViewControllerとボタンの紐付け

--- CountUpAppForBeginners/ViewController.swift
+++ CountUpAppForBeginners/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func countUpButton(_ sender: Any) {
+    }
+    
+    @IBAction func countDounButton(_ sender: Any) {
+    }
 }
 
