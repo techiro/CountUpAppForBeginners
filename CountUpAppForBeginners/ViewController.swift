7652cbbdf462135002db0f71c400b09d83c20482
techiro <techiromen0@gmail.com>
2020-08-04 18:13:39 +0900

    Initial Commit

---
+++ CountUpAppForBeginners/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountUpAppForBeginners
+//
+//  Created by TanakaHirokazu on 2020/08/04.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
