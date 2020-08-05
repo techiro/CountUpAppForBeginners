0e553f137528e318c636f0b798de1ba5d5741c26
techiro <techiromen0@gmail.com>
2020-08-05 13:57:27 +0900

    NextViewControllerの追加

---
+++ CountUpAppForBeginners/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  CountUpAppForBeginners
+//
+//  Created by TanakaHirokazu on 2020/08/05.
+//  Copyright © 2020 TanakaHirokazu. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
